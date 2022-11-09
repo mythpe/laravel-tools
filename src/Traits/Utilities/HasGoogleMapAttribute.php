0033f5a23309ae24b9b43f614c0ecfa84a7c0157
MyTh <mythpe@gmail.com>
2022-11-09 18:27:57 +0300

    google map api's

--- src/Traits/Utilities/HasGoogleMapAttribute.php
+++ src/Traits/Utilities/HasGoogleMapAttribute.php
@@ -27,9 +27,7 @@ trait HasGoogleMapAttribute
         if ($value) {
             return (string) $value;
         }
-        $lat = $this->latitude;
-        $lng = $this->longitude;
-        return $lat && $lng ? "https://www.google.com/maps/place/{$lat},{$lng}" : null;
+        return $this->getGoogleMapDirApi($this->latitude, $this->longitude);
     }
 
     /**
@@ -47,6 +45,47 @@ trait HasGoogleMapAttribute
         }
         $lat = $this->latitude;
         $lng = $this->longitude;
-        return $lat && $lng ? "https://maps.google.com/maps?q={$lat},{$lng}&hl=es&z=14&output=embed" : null;
+        //return $lat && $lng ? "https://maps.google.com/maps?q={$lat},{$lng}&hl=es&z=14&output=embed" : null;
+        return $lat && $lng ? "https://maps.google.com/maps?q=".urlencode("{$lat},{$lng}")."&hl=es&z=14&output=embed" : null;
+    }
+
+    /**
+     * @param  float|null  $lat
+     * @param  float|null  $lng
+     *
+     * @return string|null
+     */
+    public function getGoogleMapSearchApi(float|null $lat = null, float|null $lng = null): ?string
+    {
+        $lat = $lat ?: $this->latitude;
+        $lng = $lng ?: $this->longitude;
+        return $lat && $lng ? "https://www.google.com/maps/search/?api=1&query=".urlencode("{$lat},{$lng}") : null;
+    }
+
+    /**
+     * @param  float|null  $lat
+     * @param  float|null  $lng
+     *
+     * @return string|null
+     */
+    public function getGoogleMapDirApi(float|null $lat = null, float|null $lng = null): ?string
+    {
+        $lat = $lat ?: $this->latitude;
+        $lng = $lng ?: $this->longitude;
+        return $lat && $lng ? "https://www.google.com/maps/dir/?api=1&destination=".urlencode("{$lat},{$lng}") : null;
+    }
+
+    /**
+     * @param  int  $z
+     * @param  float|null  $lat
+     * @param  float|null  $lng
+     *
+     * @return string|null
+     */
+    public function getGoogleMapPlaceApi(int $z = 17, float|null $lat = null, float|null $lng = null): ?string
+    {
+        $lat = $lat ?: $this->latitude;
+        $lng = $lng ?: $this->longitude;
+        return $lat && $lng ? "https://www.google.com/maps/place/".urlencode("@{$lat},{$lng},{$z}") : null;
     }
 }
