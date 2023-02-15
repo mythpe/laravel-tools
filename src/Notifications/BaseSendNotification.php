7869d3cca92ee6fc38c44d307c68d65d746d7d9f
MyTh <mythpe@gmail.com>
2023-02-15 21:40:41 +0300

    Update BaseSendNotification.php

--- src/Notifications/BaseSendNotification.php
+++ src/Notifications/BaseSendNotification.php
@@ -154,8 +154,10 @@ class BaseSendNotification extends Notification implements ShouldQueue
         $content = str_ireplace($breaks, PHP_EOL, $content);
         $content = str_ireplace(PHP_EOL, $s, $content);
         $lines = explode($s, $content);
-        $greeting = is_array($this->greeting) ? __(...$this->greeting) : (trans_has($this->greeting) ? __($this->greeting) : $this->greeting);
-        $mail->greeting($greeting);
+        if ($this->greeting) {
+            $greeting = is_array($this->greeting) ? __(...$this->greeting) : (trans_has($this->greeting) ? __($this->greeting) : $this->greeting);
+            $mail->greeting($greeting);
+        }
         //d($lines);
         foreach ($lines as $line) {
             $mail->line($line);
