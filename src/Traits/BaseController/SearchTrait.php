a935d92ae7de55520e97b6730fe11b51ef5da579
MyTh <mythpe@gmail.com>
2023-08-12 12:21:50 +0300

    Fix

--- src/Traits/BaseController/SearchTrait.php
+++ src/Traits/BaseController/SearchTrait.php
@@ -56,10 +56,10 @@ trait SearchTrait
     /**
      * Parsing name of column
      * @param $str
-     * @param $camel
+     * @param bool $camel
      * @return string
      */
-    public function parseColumn($str, $camel = !1): string
+    public function parseColumn($str, bool $camel = !1): string
     {
         if (!$camel) {
             return Str::snake(Str::beforeLast($str, '_id'));
@@ -101,7 +101,8 @@ trait SearchTrait
                                 break;
                             }
                         }
-                    } else {
+                    }
+                    else {
                         $column = $header;
                         if ($column == $nameAttribute && !$model->isFillable($column) && $model->isFillable(locale_attribute($nameAttribute))) {
                             $insertNameColumns = !0;
@@ -124,18 +125,21 @@ trait SearchTrait
                     if ($column) {
                         if (Schema::hasColumn($this->searchTable, $column)) {
                             $this->mergeSearchColumns($column);
-                        } else {
+                        }
+                        else {
                             if (!$this->isMapQueryColumn($column)) {
                                 if (method_exists($model, $this->parseColumn($column))) {
                                     $this->mergeSearchColumns($column);
-                                } elseif (method_exists($model, $this->parseColumn($column))) {
+                                }
+                                elseif (method_exists($model, $this->parseColumn($column))) {
                                     $this->mergeSearchColumns($column);
                                 }
                             }
                         }
                     }
                 }
-            } else {
+            }
+            else {
                 $this->mergeSearchColumns($model->getFillable());
             }
         }
@@ -144,132 +148,111 @@ trait SearchTrait
             foreach ($this->getSearchColumns() as $k => $column) {
                 /** Default no custom */
                 if (is_numeric($k)) {
-                    if (
-                        // Normal
-                        Schema::hasColumn($this->searchTable, $column)
-                        // Set map from controller
-                        || $this->isMapQueryColumn($column)
-                        // Search if it has relation
-                        || method_exists($model, $this->parseColumn($column))
-                        || method_exists($model, $this->parseColumn($column, !0))
-                    ) {
-                        if ($this->isMapQueryColumn($column)) {
-                            $map = $this->getMapSearchQueryColumns($column);
-                            if (count($map) == 1 && method_exists($model, 'scope'.ucfirst($map[0]))) {
-                                $builder->orWhere(fn($q) => $q->{$map[0]}($words));
-                            } else {
-                                // d(3);
-                                $relation = ($map['relation'] ?? ($map[0] ?? Str::beforeLast($column, '_id')));
-                                $method = ($map['method'] ?? 'orWhereHas');
-                                $operator = ($map['operator'] ?? 'LIKE');
-                                $value = str_ireplace('{v}', $words, ($map['value'] ?? '%{v}%'));
-                                $column = ($map['column'] ?? null);
-                                if (is_null($column)) {
-                                    if (method_exists($model, 'getNameColumn')) {
-                                        $column = $model->getNameColumn();
-                                    } else {
-                                        $name = 'name';
-                                    }
+                    // Set map from controller
+                    if ($this->isMapQueryColumn($column)) {
+                        $map = $this->getMapSearchQueryColumns($column);
+                        if (count($map) == 1 && method_exists($model, 'scope'.ucfirst($map[0]))) {
+                            $builder->orWhere(fn($q) => $q->{$map[0]}($words));
+                        }
+                        else {
+                            // d(3);
+                            $relation = ($map['relation'] ?? ($map[0] ?? Str::beforeLast($column, '_id')));
+                            $method = ($map['method'] ?? 'orWhereHas');
+                            $operator = ($map['operator'] ?? 'LIKE');
+                            $value = str_ireplace('{v}', $words, ($map['value'] ?? '%{v}%'));
+                            $column = ($map['column'] ?? null);
+                            if (is_null($column)) {
+                                if (method_exists($model, 'getNameColumn')) {
+                                    $column = $model->getNameColumn();
                                 }
-                                // d($relation,$method,$operator,$value,$column);
-                                $builder->{$method}($relation, function (Builder $builder) use ($column, $operator, $value, $words) {
-                                    return $builder->where($column, $operator, $value);
+                                else {
+                                    $name = 'name';
+                                }
+                            }
+                            // d($relation,$method,$operator,$value,$column);
+                            $builder->{$method}($relation, function (Builder $builder) use ($column, $operator, $value, $words) {
+                                return $builder->where($column, $operator, $value);
+                            });
+                        }
+                    }
+                    elseif (Str::endsWith($column, '_id') && (method_exists($model, $this->parseColumn($column)) || method_exists($model, $this->parseColumn($column, !0)))) {
+                        // d($words,$column);
+                        $str = Str::beforeLast($column, '_id');
+                        $relations = array_unique([
+                            Str::snake($str),
+                            Str::camel($str),
+                            ucfirst(Str::camel($str)),
+                        ]);
+                        foreach ($relations as $relation) {
+                            if (
+                                method_exists($model, $relation)
+                                && ($relationModel = $model->$relation()->getModel())
+                                && method_exists($relationModel, 'getNameColumn')
+                                && Schema::hasColumn($relationModel->getTable(), ($c = $relationModel->getNameColumn()))
+                            ) {
+                                $builder->orWhere(function (Builder $builder) use ($relationModel, $relation, $c, $words) {
+                                    $builder->whereHas($relation, function (Builder $builder) use ($relationModel, $c, $words) {
+                                        // d($c, $words);
+                                        $builder->where($c, 'LIKE', "%{$words}%");
+                                        if (Helpers::hasTrait($relationModel, HasTranslatorTrait::class)) {
+                                            $availableAttributes = $relationModel->availableTranslationAttributes();
+                                            if (in_array($c, $availableAttributes)) {
+                                                $builder->orWhere(fn(Builder $t) => $t->whereHas('translator', fn($m) => $m->where('attribute', $c)->where('value', 'LIKE', "%$words%")));
+                                            }
+                                        }
+
+                                    });
                                 });
                             }
-                        } else {
+                        }
 
-                            //if (Str::endsWith($column, '_id') && !is_numeric($words)) {
-                            //    if (method_exists($model, ($relation = Str::beforeLast($column, '_id'))) && ($relationModel = $model->$relation()
-                            //                                                                                                        ->getModel()) && Schema::hasColumn($relationModel->getTable(), ($c = $relationModel->getNameColumn()))) {
-                            //        $builder->orWhere(function (Builder $builder) use ($relation, $c, $words) {
-                            //            $builder->whereHas($relation, function (Builder $builder) use ($c, $words) {
-                            //                $builder->where($c, 'LIKE', "%{$words}%");
-                            //            });
-                            //        });
-                            //    }
-                            //}
+                        // d($words,$column,$relations);
+                        // foreach($relations as $relation){
+                        //     /** @var \Illuminate\Database\Eloquent\Model $relationModel */
+                        //     if(
+                        //         method_exists($model, $relation)
+                        //         && ($relationModel = $model->$relation()->getModel())
+                        //         && Schema::hasColumn($relationModel->getTable(), ($c = $relationModel->getNameColumn()))
+                        //     ){
+                        //         $builder->orWhere(fn(Builder $b) => $b->whereHas($relation, function(Builder $builder) use ($relationModel, $words){
+                        //             $relationColumns = $relationModel->getFillable();
+                        //             // d($relationColumns, $relationModel);
+                        //             $relationColumn = null;
+                        //             foreach($relationColumns as $relationColumn){
+                        //                 // d($relationColumn, $relationModel);
+                        //                 $builder->orWhere($relationColumn, 'LIKE', "%{$words}%");
+                        //             }
+                        //             return $builder;
+                        //         }));
+                        //     }
+                        // }
 
-                            if (Str::endsWith($column, '_id')) {
-                                // d($words,$column);
-                                $str = Str::beforeLast($column, '_id');
-                                $relations = array_unique([
-                                    Str::snake($str),
-                                    Str::camel($str),
-                                    ucfirst(Str::camel($str)),
-                                ]);
-                                foreach ($relations as $relation) {
-                                    if (
-                                        method_exists($model, $relation)
-                                        && ($relationModel = $model->$relation()->getModel())
-                                        && method_exists($relationModel, 'getNameColumn')
-                                        && Schema::hasColumn($relationModel->getTable(), ($c = $relationModel->getNameColumn()))
-                                    ) {
-                                        $builder->orWhere(function (Builder $builder) use ($relationModel, $relation, $c, $words) {
-                                            $builder->whereHas($relation, function (Builder $builder) use ($relationModel, $c, $words) {
-                                                // d($c, $words);
-                                                $builder->where($c, 'LIKE', "%{$words}%");
-                                                if (Helpers::hasTrait($relationModel, HasTranslatorTrait::class)) {
-                                                    $availableAttributes = $relationModel->availableTranslationAttributes();
-                                                    if (in_array($c, $availableAttributes)) {
-                                                        $builder->orWhere(fn(Builder $t) => $t->whereHas('translator', fn($m) => $m->where('attribute', $c)->where('value', 'LIKE', "%$words%")));
-                                                    }
-                                                }
+                    }
+                    elseif (Schema::hasColumn($this->searchTable, $column)) {
+                        if ($column == 'id' && is_numeric($words)) {
+                            $builder->orWhere($column, '=', (int) $words);
+                        }
+                        else {
+                            $builder->orWhere($column, 'LIKE', "%{$words}%");
 
-                                            });
-                                        });
-                                    }
-                                }
-
-                                // d($words,$column,$relations);
-                                // foreach($relations as $relation){
-                                //     /** @var \Illuminate\Database\Eloquent\Model $relationModel */
-                                //     if(
-                                //         method_exists($model, $relation)
-                                //         && ($relationModel = $model->$relation()->getModel())
-                                //         && Schema::hasColumn($relationModel->getTable(), ($c = $relationModel->getNameColumn()))
-                                //     ){
-                                //         $builder->orWhere(fn(Builder $b) => $b->whereHas($relation, function(Builder $builder) use ($relationModel, $words){
-                                //             $relationColumns = $relationModel->getFillable();
-                                //             // d($relationColumns, $relationModel);
-                                //             $relationColumn = null;
-                                //             foreach($relationColumns as $relationColumn){
-                                //                 // d($relationColumn, $relationModel);
-                                //                 $builder->orWhere($relationColumn, 'LIKE', "%{$words}%");
-                                //             }
-                                //             return $builder;
-                                //         }));
-                                //     }
-                                // }
-
-                            } elseif ($column == 'id' && is_numeric($words)) {
-                                $builder->where($column, '=', (int) $words);
-                            } else {
-                                if (Schema::hasColumn($this->searchTable, $column)) {
-                                    $builder->orWhere($column, 'LIKE', "%{$words}%");
-                                } else {
-                                    // d($column);
-                                }
-                                if (Helpers::hasTrait($model, HasTranslatorTrait::class)) {
-                                    $availableAttributes = $model->availableTranslationAttributes();
-                                    if (in_array($column, $availableAttributes)) {
-                                        $builder->orWhere(fn(Builder $t) => $t->whereHas('translator', fn($m) => $m->where('attribute', $column)->where('value', 'LIKE', "%$words%")));
-                                    }
+                            if (Helpers::hasTrait($model, HasTranslatorTrait::class)) {
+                                $availableAttributes = $model->availableTranslationAttributes();
+                                if (in_array($column, $availableAttributes)) {
+                                    $builder->orWhere(fn(Builder $t) => $t->whereHas('translator', fn($m) => $m->where('attribute', $column)->where('value', 'LIKE', "%$words%")));
                                 }
                             }
                         }
                     }
-                } else {
+                }
+                else {
                     // d($words);
                     $builder->orWhere(function (Builder $builder) use ($column, $words) {
                         return $builder->{$column}($words);
                     });
                 }
             }
-            // d($builder->toSql());
             return $builder;
         });
-        //d($builder->toSql(), $words);
-
         return $builder;
     }
 
