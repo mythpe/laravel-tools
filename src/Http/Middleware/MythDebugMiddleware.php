4757fe159f769609fa870408c9c786701510e336
MyTh <mythpe@gmail.com>
2023-08-18 15:57:35 +0300

    Create MythDebugMiddleware.php

---
+++ src/Http/Middleware/MythDebugMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * MyTh Ahmed Faiz Copyright © 2016-2023 All rights reserved.
+ * Email: mythpe@gmail.com
+ * Mobile: +966590470092
+ * Website: https://www.4myth.com
+ * Github: https://github.com/mythpe
+ */
+
+namespace Myth\LaravelTools\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class MythDebugMiddleware
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (developmentMode()) {
+            mythAllowHeaders();
+        }
+        return $next($request);
+    }
+}
