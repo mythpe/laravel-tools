56d2620835aa6e1662fda8aa58fd9b1bf7365316
Ahmed Faiz <mythpe@gmail.com>
2024-01-28 21:05:01 +0300

    Update head.blade.php

--- src/resources/views/partials/head.blade.php
+++ src/resources/views/partials/head.blade.php
@@ -1,33 +1,11 @@
 <meta charset="utf-8">
-<meta
-    http-equiv="X-UA-Compatible"
-    content="IE=edge"
->
-<meta
-    name="author"
-    content="{!! $META_AUTHOR !!}"
->
-<meta
-    name="description"
-    content="{!! $META_DESCRIPTION !!}"
->
-<meta
-    name="keywords"
-    content="{!! $META_KEYWORDS !!}"
->
-<meta
-    name="viewport"
-    content="width=device-width, initial-scale=1"
->
-<meta
-    name="csrf-token"
-    content="{!! csrf_token() !!}"
->
-<meta
-    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui"
-    name="viewport"
->
-
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="author" content="{!! $META_AUTHOR !!}">
+<meta name="description" content="{!! $META_DESCRIPTION !!}">
+<meta name="keywords" content="{!! $META_KEYWORDS !!}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{!! csrf_token() !!}">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" name="viewport">
 <title>{!! $APP_NAME !!}@hasSection("title")
         - @yield("title")
     @endif @hasSection("extra_title")
