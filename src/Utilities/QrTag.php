73e7624562872b5ef0150e7b74fcf4a53acf27a3
Ahmed Faiz <mythpe@gmail.com>
2024-02-08 08:59:18 +0300

    ZATCA

--- src/Utilities/QrTag.php
+++ src/Utilities/QrTag.php
@@ -1,24 +1,74 @@
 <?php
-/*
- * MyTh Ahmed Faiz Copyright © 2016-2023 All rights reserved.
- * Email: mythpe@gmail.com
- * Mobile: +966590470092
- * Website: https://www.4myth.com
- * Github: https://github.com/mythpe
- */
 
 namespace Myth\LaravelTools\Utilities;
 
+use Illuminate\Support\Carbon;
+
 class QrTag
 {
-    protected int $tag;
+    /**
+     * @var
+     */
+    protected $tag;
 
-    protected string $value;
+    /**
+     * @var
+     */
+    protected $value;
 
+    /**
+     * @param $tag
+     * @param $value
+     */
     public function __construct($tag, $value)
     {
         $this->tag = $tag;
-        $this->value = (string) $value;
+        $this->value = $value;
+    }
+
+    /**
+     * @param $tag
+     * @param $value
+     *
+     * @return static
+     */
+    public static function array($tag, $value): self
+    {
+        return new static($tag, $value);
+    }
+
+    /**
+     * @param $array
+     *
+     * @return string
+     */
+    public static function toBase64(array $array): string
+    {
+        $string = base64_encode(implode('', array_map(function ($tag) {
+            return (string) $tag;
+        }, $array)));
+        //dd($string);
+        return $string;
+    }
+
+    /**
+     * Make tags as base64.
+     * @param string|null $name
+     * @param string|null $number
+     * @param string|Carbon|null $date
+     * @param string|float|null $amount
+     * @param string|float|null $tax
+     * @return string
+     */
+    public static function make(?string $name, ?string $number, string | Carbon | null $date, string | float | null $amount, string | float | null $tax)
+    {
+        return static::toBase64([
+            static::array(1, (string) $name ?: ''),
+            static::array(2, (string) $number ?: ''),
+            static::array(3, Carbon::make($date ?: '')->format("Y-m-d H:i:s")),
+            static::array(4, (string) $amount ?: ''),
+            static::array(5, (string) $tax ?: ''),
+        ]);
     }
 
     /**
@@ -26,13 +76,23 @@ class QrTag
      */
     public function __toString()
     {
-        return $this->toHex($this->getTag()).$this->toHex($this->getLength()).($this->getValue());
+        $value = (string) $this->getValue();
+
+        return $this->toHex($this->getTag()).$this->toHex($this->getLength()).($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
     }
 
     /**
      * @return int
      */
-    public function getTag(): int
+    public function getTag()
     {
         return $this->tag;
     }
@@ -45,14 +105,6 @@ class QrTag
     public function getLength()
     {
         return strlen($this->value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getValue(): string
-    {
-        return $this->value;
     }
 
     /**
