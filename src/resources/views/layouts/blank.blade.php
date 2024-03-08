acbbcce69d13d01c2a5d7d6d77629a1ff7453791
MyTh <mythpe@gmail.com>
2024-03-09 02:20:41 +0300

    fix

--- src/resources/views/layouts/blank.blade.php
+++ src/resources/views/layouts/blank.blade.php
@@ -2,15 +2,15 @@
 <html lang="{{$LOCALE}}" dir="{{$DIRECTION}}">
 <head>
     @include('4myth-tools::partials.head')
-    <link href="{{ public_path("storage/vendor/4myth/pdf-style/app.css") }}" rel="stylesheet" type="text/css">
-    <link href="{{ public_path("storage/vendor/4myth/pdf-style/app-{$DIRECTION}.css") }}" rel="stylesheet" type="text/css">
-    <link href="{{ public_path("storage/vendor/4myth/fonts/fontawesome-free/css/all.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset("storage/vendor/4myth/pdf-style/app.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset("storage/vendor/4myth/pdf-style/app-{$DIRECTION}.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset("storage/vendor/4myth/fonts/fontawesome-free/css/all.css") }}" rel="stylesheet" type="text/css">
     @stack('styles')
 </head>
 
 <body>
 @yield('content')
-<script src="{{public_path('storage/vendor/4myth/js/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('storage/vendor/4myth/js/jquery/jquery.min.js')}}"></script>
 
 @stack('scripts')
 </body>
---
+++ src/resources/views/layouts/pdf_blank.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{$LOCALE}}" dir="{{$DIRECTION}}">
+<head>
+    @include('4myth-tools::partials.head')
+    <link href="{{ public_path("storage/vendor/4myth/pdf-style/app.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ public_path("storage/vendor/4myth/pdf-style/app-{$DIRECTION}.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ public_path("storage/vendor/4myth/fonts/fontawesome-free/css/all.css") }}" rel="stylesheet" type="text/css">
+    @stack('styles')
+</head>
+
+<body>
+@yield('content')
+<script src="{{public_path('storage/vendor/4myth/js/jquery/jquery.min.js')}}"></script>
+
+@stack('scripts')
+</body>
+</html>
