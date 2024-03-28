9671720b17a02d4680338b6f4469e8a0a0bd7821
MyTh <mythpe@gmail.com>
2024-03-28 10:58:35 +0300

    Update MakeModelCommand.php

--- src/Console/Commands/MakeModelCommand.php
+++ src/Console/Commands/MakeModelCommand.php
@@ -215,7 +215,7 @@ html;
             $table->boolean(\'active\');
             $table->integer(\'order_by\');';
             $resource .= '
-            \'status_to_string\' => $model->active_to_string,';
+            \'status\' => $model->active_to_string,';
             $oldest .= '
         //$this->oldest = \'order_by\';';
 
