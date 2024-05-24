117c4cd913c49393b593cc0ff884373991b05d35
MyTh <mythpe@gmail.com>
2024-05-24 19:32:41 +0300

    2

--- src/Http/Middleware/PostmanMiddleware.php
+++ src/Http/Middleware/PostmanMiddleware.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * MyTh Ahmed Faiz Copyright © 2016-2023 All rights reserved.
+ * MyTh Ahmed Faiz Copyright © 2016-2024 All rights reserved.
  * Email: mythpe@gmail.com
  * Mobile: +966590470092
  * Website: https://www.4myth.com
--- src/resources/views/layouts/print.blade.php
+++ src/resources/views/layouts/print.blade.php
@@ -1,28 +1,13 @@
 @php
-$funcType = ($usePublicPath ?? !1) ? 'public_path' : 'asset';
+    $funcType = ($usePublicPath ?? !1) ? 'public_path' : 'asset';
 @endphp
-<!doctype html>
-<html
-    lang="{{$LOCALE}}"
-    dir="{{$DIRECTION}}"
->
+    <!doctype html>
+<html lang="{{$LOCALE}}" dir="{{$DIRECTION}}">
 <head>
     @include('4myth-tools::partials.head')
-    <link
-        href="{{ $funcType("storage/vendor/4myth/pdf-style/app.css") }}"
-        rel="stylesheet"
-        type="text/css"
-    >
-    <link
-        href="{{ $funcType("storage/vendor/4myth/pdf-style/app-{$DIRECTION}.css") }}"
-        rel="stylesheet"
-        type="text/css"
-    >
-    <link
-        href="{{ $funcType("storage/vendor/4myth/fonts/fontawesome-free/css/all.css") }}"
-        rel="stylesheet"
-        type="text/css"
-    >
+    <link href="{{ $funcType("storage/vendor/4myth/style/app.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ $funcType("storage/vendor/4myth/style/app-{$DIRECTION}.css") }}" rel="stylesheet" type="text/css">
+    <link href="{{ $funcType("storage/vendor/4myth/fonts/fontawesome-free/css/all.css") }}" rel="stylesheet" type="text/css">
     <style>
         .main-print-buttons {
             position: fixed;
