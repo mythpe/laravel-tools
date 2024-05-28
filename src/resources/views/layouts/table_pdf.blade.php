4fe2578ac5c9071904567bc95cc8f96a5985f161
MyTh <mythpe@gmail.com>
2024-05-28 20:05:01 +0300

    Update

--- src/resources/views/layouts/table_pdf.blade.php
+++ src/resources/views/layouts/table_pdf.blade.php
@@ -5,8 +5,8 @@ $funcType = ($usePublicPath ?? !1) ? 'public_path' : 'asset';
 <html lang="{{$LOCALE}}" dir="{{$DIRECTION}}">
 <head>
 @include('4myth-tools::partials.head')
-<link href="{{ $funcType("storage/vendor/4myth/style/app.css") }}" rel="stylesheet" type="text/css">
-<link href="{{ $funcType("storage/vendor/4myth/style/app-{$DIRECTION}.css") }}" rel="stylesheet" type="text/css">
+<link href="{{ $funcType("storage/vendor/4myth/pdf-style/app.css") }}" rel="stylesheet" type="text/css">
+<link href="{{ $funcType("storage/vendor/4myth/pdf-style/app-{$DIRECTION}.css") }}" rel="stylesheet" type="text/css">
 <link href="{{ $funcType("storage/vendor/4myth/fonts/fontawesome/css/all.css") }}" rel="stylesheet" type="text/css">
 </head>
 
