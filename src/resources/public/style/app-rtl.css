a47e2a468b714b765fa4887ccfc01004edde29d0
MyTh <mythpe@gmail.com>
2024-05-28 20:16:13 +0300

    12

--- src/resources/public/style/app-rtl.css
+++ src/resources/public/style/app-rtl.css
@@ -1,5 +1,5 @@
 /*
- * MyTh Ahmed Faiz Copyright © 2016-2023 All rights reserved.
+ * MyTh Ahmed Faiz Copyright © 2016-2024 All rights reserved.
  * Email: mythpe@gmail.com
  * Mobile: +966590470092
  * Website: https://www.4myth.com
