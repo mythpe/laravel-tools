b38b8036b3ebae5b9ba46c008b504fbf5fe93b84
MyTh <mythpe@gmail.com>
2024-08-03 04:01:39 +0300

    Update scripts.blade.php

--- src/resources/views/partials/scripts.blade.php
+++ src/resources/views/partials/scripts.blade.php
@@ -6,4 +6,6 @@ $usePublicPath = $usePublicPath ?? !1;
 <link href="{{ assetUrl("storage/vendor/4myth/style/app-{$DIRECTION}.css", !$usePublicPath) }}" rel="stylesheet" type="text/css">
 <link href="{{ assetUrl("storage/vendor/4myth/fonts/fontawesome/css/all.css", !$usePublicPath) }}" rel="stylesheet" type="text/css">
 
+@if(!$usePublicPath && strtolower(PHP_OS_FAMILY) != 'windows')
 <script src="{{assetUrl('storage/vendor/4myth/js/jquery/jquery.min.js', !$usePublicPath)}}"></script>
+@endif
