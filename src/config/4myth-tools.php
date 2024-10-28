4fb3ed07df9b1ccca1e2e42bc5ee4ea2903b01d3
MyTh <mythpe@gmail.com>
2024-10-28 14:28:09 +0300

    Update 4myth-tools.php

--- src/config/4myth-tools.php
+++ src/config/4myth-tools.php
@@ -266,5 +266,5 @@ return [
      * Default font family of views
      * Available: 'font-kufi', 'font-sstarabic', 'font-changa', 'font-almarai'
      */
-    'font_family_class' => 'font-almarai',
+    'font_family_class' => 'font-kufi',
 ];
