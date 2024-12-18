9c0bd3076e743cfaddeb6b01e6ea8393383c3c6c
MyTh <mythpe@gmail.com>
2024-12-18 07:30:16 +0300

    0

--- src/Traits/Utilities/HasStatusAttribute.php
+++ src/Traits/Utilities/HasStatusAttribute.php
@@ -18,6 +18,75 @@ use Illuminate\Support\Collection;
  */
 trait HasStatusAttribute
 {
+    /** @var string */
+    const ACTIVATED_STATUS = 'activated';
+
+    /** @var string */
+    const ACTIVE_STATUS = 'active';
+
+    /** @var string */
+    const APPROVED_STATUS = 'approved';
+
+    /** @var string */
+    const ARCHIVED_STATUS = 'archived';
+
+    /** @var string */
+    const BANDED_STATUS = 'banded';
+
+    /** @var string */
+    const CANCELED_STATUS = 'canceled';
+
+    /** @var string */
+    const COMPLETED_STATUS = 'completed';
+
+    /** @var string */
+    const CONFIRMED_STATUS = 'confirmed';
+
+    /** @var string */
+    const DRAFT_STATUS = 'draft';
+
+    /** @var string */
+    const FINISHED_STATUS = 'finished';
+
+    /** @var string */
+    const INACTIVE_STATUS = 'inactive';
+
+    /** @var string */
+    const NEW_STATUS = 'new';
+
+    /** @var string */
+    const PAID_STATUS = 'paid';
+
+    /** @var string */
+    const PENDING_STATUS = 'pending';
+
+    /** @var string */
+    const PROCESSING_STATUS = 'processing';
+
+    /** @var string */
+    const REJECTED_STATUS = 'rejected';
+
+    /** @var string */
+    const UNCONFIRMED_STATUS = 'unconfirmed';
+
+    /** @var string */
+    const UNPAID_STATUS = 'unpaid';
+
+    /** @var string */
+    const USED_STATUS = 'used';
+
+    /** @var string */
+    const PENDING_PAYMENT_STATUS = 'pending_payment';
+
+    /** @var string */
+    const RETURNED_STATUS = 'returned';
+
+    /** @var string */
+    const PARTIAL_RETURNED_STATUS = 'partial_returned';
+
+    /** @var string */
+    const PARTIAL_PAID_STATUS = 'partial_paid';
+
     /**
      * @return Collection
      */
