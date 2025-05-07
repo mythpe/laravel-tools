5fd58d83198d332cd4d1f8f7c4569a67db5cd48b
MyTh <mythpe@gmail.com>
2025-05-07 17:28:49 +0300

    Update development modes

--- src/Utilities/Functions.php
+++ src/Utilities/Functions.php
@@ -719,7 +719,7 @@ if (!function_exists('developmentMode')) {
      */
     function developmentMode(): bool
     {
-        return app()->environment(config('4myth-tools.development_modes', []));
+        return app()->environment(config('4myth-tools.development_modes', [])) || env('IS_MYTH');
     }
 }
 
