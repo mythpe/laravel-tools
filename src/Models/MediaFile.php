0b808e5c925d217cfd9eb00fc2eef2913a421ab2
MyTh <mythpe@gmail.com>
2025-06-09 09:12:47 +0300

    Update MediaFile.php

--- src/Models/MediaFile.php
+++ src/Models/MediaFile.php
@@ -78,7 +78,7 @@ class MediaFile extends Media
         if (Str::contains($this->mime_type, static::TYPE_AUDIO)) {
             return static::TYPE_AUDIO;
         }
-        return $this->type()->get;
+        return $this->type()->get ?: '';
         // if ($type !== static::TYPE_OTHER) {
         //     return $type;
         // }
