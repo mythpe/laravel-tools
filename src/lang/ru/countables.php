d137db97d17f2b8520f40753b9d02eb678460853
MyTh <mythpe@gmail.com>
2026-01-15 07:22:26 +0300

    Ru lang

---
+++ src/lang/ru/countables.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * MyTh Ahmed Faiz Copyright © 2016-2024 All rights reserved.
+ * Email: mythpe@gmail.com
+ * Mobile: +966590470092
+ * Website: https://www.4myth.com
+ * Github: https://github.com/mythpe
+ */
+
+return [
+    'Ages'              => '{0}-|[1]Один год|[2,*]:age лет',
+    'Days'              => '{0}-|[1]Один день|[2,*]:days дней',
+    'Minutes'           => '{0}-|[1]Одна минута|[2,*]:minutes минут',
+    'Hours'             => '{0}-|[1]Один час|[2,*]:hours часов',
+    'Months'            => '{0}-|[1]Один месяц|[2,*] :months месяца',
+    'OrderCounts'       => '{0}Нет заказов|{1} Один заказ|[2,*] :count заказов',
+    'PluralizedErrors'  => '[0]Предоставленные данные недействительны|[1](и еще одна ошибка)|[2](и еще :e ошибок)',
+    'ReservationCounts' => '{0}Нет бронирований|{1} Одно бронирование|[2,*] :count бронирований',
+    'Years'             => '{0}-|[1]Один год|[2,*]:years лет',
+];
---
+++ src/lang/ru/postman.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * MyTh Ahmed Faiz Copyright © 2016-2024 All rights reserved.
+ * Email: mythpe@gmail.com
+ * Mobile: +966590470092
+ * Website: https://www.4myth.com
+ * Github: https://github.com/mythpe
+ */
+
+return [
+    'description'  => 'Описание приложения',
+    'folder'       => [
+        'Controller::class' => [
+            'name'        => 'Пример',
+            'description' => 'Пример',
+        ],
+        'auth_description'  => 'Пример',
+        'gust_description'  => 'Пример',
+    ],
+    'items'        => [
+        'Controller::class' => [
+            'Method name' => 'Пример',
+        ],
+    ],
+    'descriptions' => [
+        'Controller::class' => [
+            'Method name' => 'Пример',
+        ],
+    ],
+];
