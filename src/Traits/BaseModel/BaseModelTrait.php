0a9a2935874e7daa6e10572deadc5f9f2d6c6e5e
MyTh <mythpe@gmail.com>
2026-05-29 12:34:32 +0300

    Copyright

--- src/Traits/BaseModel/BaseModelTrait.php
+++ src/Traits/BaseModel/BaseModelTrait.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * MyTh Ahmed Faiz Copyright © 2026. All rights reserved.
+ * Email: mythpe@gmail.com
+ * Mobile: +966590470092
+ * Website: https://www.4myth.com
+ * GitHub: https://github.com/mythpe
+ */
 
 namespace Myth\LaravelTools\Traits\BaseModel;
 
